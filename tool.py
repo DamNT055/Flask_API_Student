018448604c3d73c99110b5070d820b1206e1ff27
DamNT055 <89400030+DamNT055@users.noreply.github.com>
2022-08-28 14:05:32 +0700

    push via git

--- tool.py
+++
@@ -1,9 +0,0 @@
-from model import db, StudentModel
-import json
-
-
-def SaveListJson():
-    students = StudentModel.query.all()
-    json_object = json.dumps(students, indent = 3)
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
